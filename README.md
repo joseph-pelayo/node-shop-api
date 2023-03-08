c2dda94788430554ccd2a3b77a62149c9c0c496f
Joseph Pelayo <jeanjoseph.pelayo@gmail.com>
2023-03-08 19:55:47 +0400

    github repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# Build an API REST for an e-commerce store with Node.js, Express.js and MongoDb (CRUD)
---
+++ app.js
@@ -0,0 +1,82 @@
+/*=============================================*/
+/*==== Import required modules
+/*=============================================*/
+
+// Include the Express library
+const express = require('express');
+
+// Import the Mongoose module
+const mongoose = require('mongoose');
+
+// Import MongoDB database credentials
+const cnx = require('./config/db.config');
+
+
+/*=============================================*/
+/*==== Configure the application
+/*=============================================*/
+
+// Instantiate an Express object
+const app = express();
+
+
+
+/*==================================================*/
+/*==== Create routes for HTTP methods (Endpoints)
+/*==================================================*/
+
+// Import routing modules
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+
+
+
+/*==================================================*/
+/*==== Middlewares
+/*==================================================*/
+
+// Set a basic GET request route for testing purposes
+app.get('/', (req, res) => {
+
+    res.status(200).json({
+
+        message: 'Welcome to the root of your favorite API REST!'
+    });
+
+});
+
+
+// Body parser
+app.use(express.json());
+
+// Follow the specified route if the requested path is matched
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+
+
+/*=============================================*/
+/*==== Database connection
+/*=============================================*/
+
+// Suppress the warning about the change that will be
+// introduced in Mongoose 7 to the default value of 'strictQuery'
+mongoose.set('strictQuery', false);
+
+// MongoAtlas connection URI
+const mongoAtlasUri = `mongodb+srv://${cnx.username}:${cnx.password}@${cnx.cluster}.ysvpzhj.mongodb.net/${cnx.database}?retryWrites=true&w=majority`;
+
+// Connect the MongoDB cluster to our server using Mongoose
+mongoose
+    .connect(mongoAtlasUri, {
+
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    })
+    .then(() => console.log("Mongoose is connected to MongoDB Atlas"))
+    .catch((err) => console.error(err.message));
+
+
+// Make the entire module app available for import elsewhere
+module.exports = app;
+
